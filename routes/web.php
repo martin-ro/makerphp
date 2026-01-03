19d6afab5806becbea954b96b41aa23bdab74442
Martin Ro <martin-ro@users.noreply.github.com>
2026-01-03 16:09:14 +0800

    --wip-- [skip ci]

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/test-exception', function () {
-    throw new \Exception('This is a test exception');
-});
